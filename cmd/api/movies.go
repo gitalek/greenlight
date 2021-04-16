c89baafa81b7241001ae49fdb85d38898fd81cac
Alexander <shalek.edu@mail.ru>
2021-04-16 21:49:21 +1000

    controllers(movies): user readIDParam helper

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/julienschmidt/httprouter"
 	"net/http"
-	"strconv"
 )
 
 // createMovieHandler for the "POST /v1/movies" endpoint.
@@ -14,10 +12,8 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 // showMovieHandler for the "GET /v1/movies/:id" endpoint.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
+	id, err := app.readIDParam(r)
+	if err != nil {
 		http.NotFound(w, r)
 		return
 	}
