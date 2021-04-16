48bb37b33ddf187fac65975792cc4466b2e20b8b
Alexander <shalek.edu@mail.ru>
2021-04-16 23:51:55 +1000

    envelope json response

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -19,8 +19,10 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
+type envelope map[string]interface{}
+
 // writeJSON helper for sending responses.
-func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.MarshalIndent(data, "", "  ")
 	if err != nil {
 		return err
