7d6315f8d479fb241ff4fc5b49bb73e95a7a9f9e
Alexander <shalek.edu@mail.ru>
2021-05-01 19:06:19 +1000

    handlers (createMovieHandler): decode json input

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"greenlight/internal/data"
 	"net/http"
@@ -9,7 +10,20 @@ import (
 
 // createMovieHandler for the "POST /v1/movies" endpoint.
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new movie")
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
 
 // showMovieHandler for the "GET /v1/movies/:id" endpoint.
