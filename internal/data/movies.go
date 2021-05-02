c110dcce8f65ab0cd25c26e0886d789533e14cbb
Alexander <shalek.edu@mail.ru>
2021-05-03 01:24:28 +1000

    models(movie): implement Insert method

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"database/sql"
+	"github.com/lib/pq"
 	"greenlight/internal/validator"
 	"time"
 )
@@ -38,7 +39,14 @@ type MovieModel struct {
 }
 
 func (m MovieModel) Insert(movie *Movie) error {
-	return nil
+	query := `
+		INSERT INTO movies (title, year, runtime, genres)
+		VALUES ($1, $2, $3, $4)
+		RETURNING id, created_at, version`
+
+	args := []interface{}{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
 }
 
 func (m Movie) Get(id int64) (*Movie, error) {
