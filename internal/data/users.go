045bb21b403ab60c6d40f946c26c4f216bf1d2bf
Alexander <shalek.edu@mail.ru>
2021-05-08 18:37:40 +1000

    validation: add validation checks (User, password)

--- internal/data/users.go
+++ internal/data/users.go
@@ -3,6 +3,7 @@ package data
 import (
 	"database/sql"
 	"errors"
+	"greenlight/internal/validator"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
@@ -47,6 +48,36 @@ func (p *password) Matches(plainTextPassword string) (bool, error) {
 		}
 	}
 	return true, nil
+}
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlainText(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
+
+func ValidateUser(v *validator.Validator, user *User) {
+	v.Check(user.Name != "", "name", "must be provided")
+	v.Check(len(user.Name) <= 500, "name", "must not be more than 500 bytes long")
+
+	ValidateEmail(v, user.Email)
+	if user.Password.plaintext != nil {
+		ValidatePasswordPlainText(v, *user.Password.plaintext)
+	}
+
+	// If the password hash is ever nil, this will be due to a logic error in our
+	// codebase (probably because we forgot to set a password for the user). It's a
+	// useful sanity check to include here, but it's not a problem with the data
+	// provided by the client. So rather than adding an error to the validation map we
+	// raise a panic instead.
+	if user.Password.hash == nil {
+		panic("missing password hash for user")
+	}
 }
 
 type UserModel struct {
