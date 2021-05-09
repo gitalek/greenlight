4d607974c7871f54c0141c139d1e1db1336de473
Alexander <shalek.edu@mail.ru>
2021-05-09 20:58:07 +1000

    models(token): define TokenModel and New, Insert, DeleteForUser methods

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,9 +1,12 @@
 package data
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/sha256"
+	"database/sql"
 	"encoding/base32"
+	"greenlight/internal/validator"
 	"time"
 )
 
@@ -38,4 +41,50 @@ func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error
 	token.Hash = hash[:]
 
 	return token, nil
+}
+
+func ValidateTokenPlaintext(v *validator.Validator, tokenPlaintext string) {
+	v.Check(tokenPlaintext != "", "token", "must be provided")
+	v.Check(len(tokenPlaintext) == 26, "token", "must be 26 bytes long")
+}
+
+type TokenModel struct {
+	DB *sql.DB
+}
+
+func (t TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token, err := generateToken(userID, ttl, scope)
+	if err != nil {
+		return nil, err
+	}
+
+	err = t.Insert(token)
+	return token, err
+}
+
+func (t TokenModel) Insert(token *Token) error {
+	query := `
+		INSERT INTO tokens (hash, user_id, expiry, scope)
+		VALUES ($1, $2, $3, $4)`
+
+	args := []interface{}{token.Hash, token.UserID, token.Expiry, token.Scope}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := t.DB.ExecContext(ctx, query, args...)
+	return err
+}
+
+// DeleteAllForUser deletes all tokens for a specific user and scope.
+func (t TokenModel) DeleteAllForUser(userID int64, scope string) error {
+	query := `
+		DELETE FROM tokens
+		WHERE user_id = $1 AND scope = $2`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := t.DB.ExecContext(ctx, query, scope, userID)
+	return err
 }
