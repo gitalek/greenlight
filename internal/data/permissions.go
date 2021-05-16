3dcbfef6696ca3e3d7673bc72a1a02cc28bc7d45
Alexander <shalek.edu@mail.ru>
2021-05-16 14:38:03 +1000

    models: define PermissionModel and GetAllForUser method

---
+++ internal/data/permissions.go
@@ -0,0 +1,58 @@
+package data
+
+import (
+	"database/sql"
+	"context"
+	"time"
+)
+
+type Permissions []string
+
+func (p Permissions) Include(code string) bool {
+	for _, c := range p {
+		if code == c {
+			return true
+		}
+	}
+	return false
+}
+
+type PermissionModel struct {
+	DB *sql.DB
+}
+
+func (p PermissionModel) GetAllForUser(userID int64) (Permissions, error) {
+	query := `
+ 		SELECT permissions.code
+ 		FROM permissions
+ 		INNER JOIN users_permissions ON users_permissions.permission_id = permission_id
+ 		INNER JOIN users ON users_permissions.user_id = users.id
+ 		WHERE user_id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := p.DB.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var permissions Permissions
+
+	for rows.Next() {
+		var permission string
+
+		err := rows.Scan(&permission)
+		if err != nil {
+			return nil, err
+		}
+
+		permissions = append(permissions, permission)
+	}
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return permissions, nil
+}
