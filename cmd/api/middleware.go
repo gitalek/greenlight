37086f95a638a8e0a5a1d69e55e9d5a535a8bc3e
Alexander <shalek.edu@mail.ru>
2021-05-17 23:46:13 +1000

    middleware(enableCORS): update to support multiple origins

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -175,7 +175,16 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Vary", "Origin")
+		origin := r.Header.Get("Origin")
+		if origin != "" && len(app.config.cors.trustedOrigins) != 0 {
+			for _, trusted := range app.config.cors.trustedOrigins {
+				if origin == trusted {
+					w.Header().Set("Access-Control-Allow-Origin", trusted)
+					break
+				}
+			}
+		}
 
 		next.ServeHTTP(w, r)
 	})
