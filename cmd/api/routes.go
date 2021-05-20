4b36be0faac3a19f51cb6229925ed01d31270264
Alexander <shalek.edu@mail.ru>
2021-05-20 21:14:01 +1000

    routing: use metrics middleware

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -28,5 +28,5 @@ func (app *application) routes() http.Handler {
 	// Metrics routes.
 	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 
-	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router))))
+	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
 }
